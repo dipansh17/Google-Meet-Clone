c7d5aa5861bdfbc8b870d606c48136961f6e215e
dipansh17 <dipanshmalhotra11@gmail.com>
2023-07-12 16:29:36 +0530

    Deployment

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import { React, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 const HomePage = () => {
   const [value, setValue] = useState();
